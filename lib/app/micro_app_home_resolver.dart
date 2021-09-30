2fefb78c8e99c851c0d213885b9e09cd873dcce7
CadisRaziel <vitorcsharp@hotmail.com>
2021-09-30 09:44:35 -0300

    criando botão personalizado

---
+++ lib/app/micro_app_home_resolver.dart
@@ -0,0 +1,17 @@
+
+import 'package:micro_app_home/app/pages/home_page.dart';
+import 'package:micro_core/app/micro_core_utils.dart';
+import 'package:micro_core/app/microapp.dart';
+
+class MicroAppHomeResolver implements MicroApp{
+  @override
+  // TODO: implement microAppName
+  String get microAppName => 'micro_app_home';
+
+  @override
+  // TODO: implement routes
+  Map<String, WidgetBuilderArgs> get routes => {
+    '/home': (context, args) => const HomePage(),
+  };
+  
+}
