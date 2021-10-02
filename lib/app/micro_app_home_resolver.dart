071f21b8f50e51f4f0417b8619cc4cbbdf43cebf
vitor.Brussolo.zerbato.Houseasy <vitor.zerbato@houseasy.net>
2021-10-02 09:51:54 -0300

    apenas um teste de config do git

--- lib/app/micro_app_home_resolver.dart
+++ lib/app/micro_app_home_resolver.dart
@@ -1,9 +1,8 @@
-
 import 'package:micro_app_home/app/pages/home_page.dart';
 import 'package:micro_core/app/micro_core_utils.dart';
 import 'package:micro_core/app/microapp.dart';
 
-class MicroAppHomeResolver implements MicroApp{
+class MicroAppHomeResolver implements MicroApp {
   @override
   // TODO: implement microAppName
   String get microAppName => 'micro_app_home';
@@ -11,7 +10,9 @@ class MicroAppHomeResolver implements MicroApp{
   @override
   // TODO: implement routes
   Map<String, WidgetBuilderArgs> get routes => {
-    '/home': (context, args) => const HomePage(),
-  };
-  
+        '/home': (context, args) => const HomePage(),
+      };
+
+  //*...
+
 }
